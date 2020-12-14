ff7eb9d8b4fd9a51887c49326fe0f7072cfea296
komadmi <dmitriy.comarov@gmail.com>
2020-12-14 09:27:44 +0300

    Error catch

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import React from "react";
 declare const VERSION: string; 
 
 function App() {
+  let versionToDisplay = "unknown";
+  try {
+    versionToDisplay = VERSION;
+  } catch (error) {
+    console.log("Cannot get version of application.");
+  }
   return (
-    <div>{`Version: ${VERSION}`}</div>
+    <div>{`Version: ${versionToDisplay}`}</div>
   );
 }
 
